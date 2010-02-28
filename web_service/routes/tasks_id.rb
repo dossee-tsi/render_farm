4a53f93d300dcd7501e43f86a33d613b33598873
dremora <none@none>
2010-02-28 05:08:43 +0200

    Fixed status bug, added response to "open" action

--- web_service/routes/tasks_id.rb
+++ web_service/routes/tasks_id.rb
@@ -4,6 +4,8 @@ module RenderFarm
 
     def set_task_status(id, status)
       task = Task.first(:id => id)
+      throw_bad_request unless task
+      throw_bad_request if task.status == status
       task.status = status
       task.modified = Time.now
       throw_bad_request unless task.save
@@ -18,15 +20,17 @@ module RenderFarm
 
   get '/tasks/:id' do
     local_area!
-    # status, xml, blend, output
+    task = Task.first(:id => params[:id])
+    throw_bad_request unless task
+    json task.attributes
   end
 
   post '/tasks/:id' do
     local_area!
     requires params, :action
     case params[:action]
-    when 'accept' then set_task_status(params[:id], :accept)
-    when 'reject' then set_task_status(params[:id], :reject)
+    when 'accept' then set_task_status(params[:id], :accepted)
+    when 'reject' then set_task_status(params[:id], :rejected)
     else throw_bad_request
     end
   end
